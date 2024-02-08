e2fa291d236cd89bdef21e8b748bfc7a061684e5
anassjamad <220437@student.glu.nl>
2024-02-08 12:12:28 +0100

    V2

--- P7-1 FORMAL ELEMENTS OF GAME DESIGN/Assets/Scripts/Bandit behaviour.cs
+++ P7-1 FORMAL ELEMENTS OF GAME DESIGN/Assets/Scripts/Bandit behaviour.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GLU.steering;
 
 public class Banditbehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Steering steering;
+    [SerializeField] private GameObject target;
     void Start()
     {
-        
+
+        steering = GetComponent<Steering>();
+
+        List<IBehavior> behaviors = new List<IBehavior>();
+
+        behaviors.Add(new Pursue(target));
+        behaviors.Add(new FollowWall());
+        behaviors.Add(new AvoidWall());
+
+        steering.SetBehaviors(behaviors);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
